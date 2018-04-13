ad0ee23b8528528317011854512f580f7f3d656c
unknown <zubairuzb@gmail.com>
2018-04-13 14:00:12 -0700

    first commit

---
+++ homepage.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+<head><title>Hng Internship Stage 1</title>
+<link rel="stylesheet" type="text/css" href="css/styles.css"/>
+</head>
+<body>
+<h1>HNG INTERNSHIP4 STAGE 1</h1>
+<p>I am very happy to enroll in this amazing programme where i can boost my skills to the next level</p>
+
+<div class="ctime">
+<?php
+echo "The time now is " . date("h:i:sa");
+?> 
+</div>
+<div class="side">
+<h3>Thanks To:</h3>
+<ul>
+<li>Hotels.ng</li>
+<li>Slack</li>
+<li>Team Viewer</li>
+<li>Xampp</li>
+</ul>
+</div>
+<footer class="bfooter">
+&copy; ZB Computers Inc. Zubairuzb@gmail.com
+</footer>
+</body>
+</html>
